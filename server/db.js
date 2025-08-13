033adf00ebef10b293140662298ab1cb3d0414bc
IT Support <itsupport@cmm-pumch.gov.mo>
2025-08-13 12:23:22 +0800

    update

--- server/db.js
+++ server/db.js
@@ -8,7 +8,8 @@ const pool = mysql.createPool({
   database: 'notes_db',
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  timezone: 'Z'
 });
 
 // Function to create and update tables
